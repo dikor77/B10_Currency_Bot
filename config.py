adfadbfe1495c687fd9bcca1d60a05359743c2ab
dikor77 <dakovyazin@gmail.com>
2022-08-30 16:58:37 +0600

    first commit

---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN = ''
+API_KEY = ''
+
+payload = {}
+headers= {
+  "apikey": API_KEY
+}
+
+keys = {
+    'рубль' : 'RUB',
+    'лира' : 'TRY',
+    'доллар' : 'USD',
+    'тенге' : 'KZT',
+    'евро' : 'EUR',
+}
